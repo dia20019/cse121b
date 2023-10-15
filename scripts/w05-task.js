8c9f4bc749dc63a9e18180f26bbe4f48d854fb4b
dia20019 <dia20019@byui.edu>
2023-10-14 23:32:54 -0600

    w5

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,23 +1,66 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-
-
+const templesElement = document.querySelector("#temples");
+let templeList = [];
 /* async displayTemples Function */
-
-
-
-
+function displayTemples (templeList) {
+    templeList.forEach( (element) => {
+    let templeName = document.createElement("h3");
+    let newImage = document.createElement("img");
+    newImage.setAttribute("src", element.imageUrl);
+    newImage.setAttribute("alt", element.location );
+    let article = document.createElement("article");
+    article.append(templeName, newImage);
+    templesElement.appendChild(article);
+});
+}
 /* async getTemples Function using fetch()*/
-
-
+const getTemples = async () => {
+    try {
+        const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        templeList = data; // Set the global templeList to the fetched data
+        displayTemples(templeList); // Call displayTemples function
+        console.log(templeList);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
 /* reset Function */
-
-
+function reset() {
+    document.getElementById("temples").innerHTML = "";
+  }
 /* sortBy Function */
+function sortBy (temples) {
+    reset ();
+    const filter = document.querySelector("#sortBy").value;
+    switch (filter) {
+        case "utah":
+            const utahTemples = temples.filter(temp => temp.location.includes("Utah"));
+            displayTemples(utahTemples);
+            break;
+        
+        case "notutah":
+            const notUtahTemples = temples.filter (temp => !temp.location.includes("Utah"));
+            displayTemples(notUtahTemples);
+            break;
+        
+        case "older":
+            const olderTemples = temples.filter(temp => new Date(temp.dedicated) < new Date(1950, 0, 1));
+            displayTemples(olderTemples);
+            break;
 
-
-
-getTemples();
-
+        case "all":
+            displayTemples(temples);
+            break;
+    }
+}
 /* Event Listener */
+document.querySelector("#sortBy").addEventListener("change", () => {
+    sortBy(templeList);
+});
+getTemples();
