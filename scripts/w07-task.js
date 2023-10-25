a6dc1edbd206db88e755ecff62b6411d9df76197
dia20019 <dia20019@byui.edu>
2023-10-24 22:56:06 -0600

    w7b

--- scripts/w07-task.js
+++ scripts/w07-task.js
@@ -14,4 +14,39 @@ console.log(await res.json());
 
 new Def.Autocompleter.Search('condition',
  'https://clinicaltables.nlm.nih.gov/api/conditions/v3/search');
-              
+          
+import autoComplete from "@tarekraafat/autocomplete.js";
+
+const autoCompleteJS = new autoComplete({ config });
+
+var heart_terms = ['heart disease', 'heart murmur', 'heart attack', 'heart doctor', 'high blood pressure', 'high cholesterol'];
+var brain_terms = ['brain tumor', 'brain aneurism', 'struck', 'headache', 'seizure', 'neuropaty'];
+var lung_terms = ['lung issue', 'breathing problem', 'shortness of breath', 'wheezing', 'asfixia', 'choking'];
+var endocrinology_terms = ['diabetes', 'hypothyroidism', 'insuline resistance', 'TRT', 'estrogen', 'progesteron'];
+
+
+
+
+
+function autocompleteMatch(input) {
+  if (input == '') {
+    return [];
+  }
+  var reg = new RegExp(input)
+  return search_terms.filter(function(term) {
+	  if (term.match(reg)) {
+  	  return term;
+	  }
+  });
+}
+
+function showResults(val) {
+  res = document.getElementById("result");
+  res.innerHTML = '';
+  let list = '';
+  let terms = autocompleteMatch(val);
+  for (i=0; i<terms.length; i++) {
+    list += '<li>' + terms[i] + '</li>';
+  }
+  res.innerHTML = '<ul>' + list + '</ul>';
+}
